fd9c85125cb48bc6e8b1f28f0c8469df15cdbaf8
THANAPHOOM AUNCHIT <me@thanaphoom.dev>
2025-01-19 22:09:59 +0700

    ✅ (Test): Add preprocess test
     Add preprocess test

---
+++ test/preprocess.test.ts
@@ -0,0 +1,107 @@
+import { IProvince, IWord } from '../src/utils/preprocess.d';
+import { preprocess, preprocess_word } from '../src/utils/preprocess';
+
+const data: IProvince[] = [
+    [
+        0,
+        [
+            [
+                1,
+                [
+                    [2, 81120],
+                    [3, 81120],
+                    [4, 81170],
+                ],
+            ],
+        ],
+    ],
+];
+
+describe('Preprocess Word Test', () => {
+    it('should return an empty array when data is empty', () => {
+        const data: IWord = {
+            data: [],
+            lookup: '',
+            words: '',
+        };
+
+        const result = preprocess_word(data);
+        expect(result).toEqual([]);
+    });
+
+    it('should transform text based on lookup and words data for Thai characters', () => {
+        const data: IWord = {
+            data: ['C', 'AB', 'ABD', 'ABE', 'AF'],
+            lookup: '',
+            words: 'คลอง|ท่อม|กระบี่|เหนือ|ใต้|พน',
+        };
+
+        const result = preprocess_word(data);
+        expect(result).toEqual([
+            'กระบี่',
+            'คลองท่อม',
+            'คลองท่อมเหนือ',
+            'คลองท่อมใต้',
+            'คลองพน',
+        ]);
+    });
+
+    it('should transform text based on lookup and words data for Eng characters', () => {
+        const data: IWord = {
+            data: ['ฃ', 'ก ข', 'ก ข ค', 'ก ข ฅ', 'ก ฆ'],
+            lookup: '',
+            words: 'Khlong|Thom|Krabi|Nuea|Tai|Phon',
+        };
+
+        const result = preprocess_word(data, true);
+        expect(result).toEqual([
+            'Krabi',
+            'Khlong Thom',
+            'Khlong Thom Nuea',
+            'Khlong Thom Tai',
+            'Khlong Phon',
+        ]);
+    });
+});
+
+describe('Preprocess Test', () => {
+    it('should return an empty array when data is empty', () => {
+        const words = [
+            'กระบี่',
+            'คลองท่อม',
+            'คลองท่อมเหนือ',
+            'คลองท่อมใต้',
+            'คลองพน',
+        ];
+
+        const data: IProvince[] = [];
+        const result = preprocess(data, words);
+        expect(result).toEqual([]);
+    });
+
+    it('should transform text based on words and data for Thai Address', () => {
+        const words = [
+            'กระบี่',
+            'คลองท่อม',
+            'คลองท่อมเหนือ',
+            'คลองท่อมใต้',
+            'คลองพน',
+        ];
+
+        const result = preprocess(data, words);
+        expect(result.length).toBe(3);
+    });
+
+    it('should transform text based on words and data for Eng Address', () => {
+        const words = [
+            'Krabi',
+            'Khlong Thom',
+            'Khlong Thom Nuea',
+            'Khlong Thom Tai',
+            'Khlong Phon',
+        ];
+
+        const result = preprocess(data, words);
+        expect(result.length).toBe(3);
+    });
+});
